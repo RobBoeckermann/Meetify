9710eb19bc5a111842bf989f7d5e344a4947017a
Rob Boeckermann <robboeckermann@gmail.com>
2020-10-03 19:16:40 -0400

    Hackathon Update
    Added Spotipy documentation.
    Removed secret keys (use env vars instead).
    Added SQL commands to delete/write liked songs to database.

--- Hackathon_Playlist_Intersection/main.py
+++ Hackathon_Playlist_Intersection/main.py
@@ -1,58 +1,42 @@
 if __name__ == '__main__':
+    import os
     import spotipy
     from spotipy.oauth2 import SpotifyOAuth
-    import spotipy.util as util
-    # import mysql.connector
-    # cnx = mysql.connector.connect(user='scott', password='password',
-    #                               host='127.0.0.1',
-    #                               database='employees')
-    # cnx.close()
+    import mysql.connector
+
+    print(os.getenv('MEETIFY_SQL_PASS'))
+    cnx = mysql.connector.connect(user='root', password=os.getenv('MEETIFY_SQL_PASS'),
+                                  host='35.221.58.248',
+                                  database='meetify')
+    cursor = cnx.cursor()
 
     from pprint import pprint
 
-
-    user1_sp_api_client = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id="f0a9dbb3fb9c4d22b20b5a00cca0e4b2",
-                                               client_secret="14790579a73143cb982f3fc8b41caf2c",
+    user1_sp_api_client = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=os.getenv('SPOTIPY_CLIENT_ID'),
+                                               client_secret=os.getenv('SPOTIPY_CLIENT_SECRET'),
                                                redirect_uri="http://localhost",
                                                scope="user-library-read")) #, "playlist-modify-private"
     pprint(user1_sp_api_client.me())
-
-    #If you are reluctant to immortalize your app credentials in your source code, you can set environment variables like so (use SET instead of export on Windows):
-    # SET SPOTIPY_CLIENT_ID = 'f0a9dbb3fb9c4d22b20b5a00cca0e4b2'
-    # SET SPOTIPY_CLIENT_SECRET = '14790579a73143cb982f3fc8b41caf2c'
-    # SET SPOTIPY_REDIRECT_URI = 'http://localhost' #needs to match redirect uri in project setting on spotify developer dashboard. This is the page the user will be redirected to after authorizing Meetify to access their spotify account.
-
+    userid = user1_sp_api_client.me()['id']
 
     offset = 0
     user1_liked_songs = []
-    while (offset < 300):
+    while (offset < 5000):
         user1_saved_tracks = user1_sp_api_client.current_user_saved_tracks(limit=50, offset=offset)
         for item in user1_saved_tracks['items']:
             user1_liked_songs.append(item["track"]["id"]);
-        offset = offset + 50;
+        offset = offset + 50
+
+    query = ("DELETE FROM user_liked_songs WHERE user_id='" + userid + "';")
+    cursor.execute(query)
     for id in user1_liked_songs:
-        print(id)
+        query = ("INSERT INTO user_liked_songs (user_id, song_id) "
+                "VALUES (%s, %s)")
+        data_query = (userid, id)
+        cursor.execute(query, data_query)
+    cnx.commit()
 
 
+    cursor.close()
+    cnx.close()
 
-    user2_sp_api_client = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id="f0a9dbb3fb9c4d22b20b5a00cca0e4b2",
-                                                                    client_secret="14790579a73143cb982f3fc8b41caf2c",
-                                                                    redirect_uri="http://localhost",
-                                                                    scope="user-library-read"))
-    pprint(user2_sp_api_client.me())
-
-    offset = 0
-    user2_liked_songs = []
-    while (offset < 300):
-        user2_saved_tracks = user2_sp_api_client.current_user_saved_tracks(limit=50, offset=offset)
-        for item in user2_saved_tracks['items']:
-            user2_liked_songs.append(item["track"]["id"]);
-        offset = offset + 50;
-    for id in user2_liked_songs:
-        print(id)
-
-
-    intersection = [value for value in user1_liked_songs if value in user2_liked_songs]
-    print("intersection:")
-    for id in intersection:
-        print(id)
