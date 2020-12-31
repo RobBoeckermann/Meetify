0cf6c612cd1bace703c8f662f30c6c2f938a562d
Dustin Seger <dustin.seger@hotmail.com>
2020-12-31 10:38:56 -0500

    UI - Clean up profile warnings and add gradients

--- front_end/src/Account.jsx
+++ front_end/src/Account.jsx
@@ -3,7 +3,6 @@ import {
   Card,
   CardMedia,
   Grid,
-  Paper,
   Typography,
 } from '@material-ui/core'
 
@@ -15,6 +14,9 @@ import { theme } from './theme.js'
 const MAIN_CARD_MAX_WIDTH = '700px'
 const MARGIN = '8px'
 
+const HEADER_GRADIENT = theme.getGradient([theme.palette.background.default, theme.palette.primary.dark])
+const BODY_GRADIENT = theme.getGradient([theme.palette.background.default, theme.palette.secondary.dark])
+
 // == TEMP TEST STUFF ==
 
 const PROFILE_IMG_SIZE = theme.images.squareImageHeight
@@ -22,7 +24,7 @@ const PROFILE_IMG_SIZE = theme.images.squareImageHeight
 const TEST_INFO = {
   username: 'dougydougdoug',
   displayName: 'Doug Douglas',
-  description: 'Hey! The name\'s Doug, but you can call be "D-D-D-Doug in da Hiz House". Please talk to me.',
+  description: 'Hey! The name\'s Doug, but you can call be "D-D-D-Doug in da Hiz House". Please talk to me. '.repeat(10),
   status: 'Chillin\'',
   profilePicUrl: 'https://www.kindpng.com/picc/m/78-785827_user-profile-avatar-login-account-male-user-icon.png',
 }
@@ -37,7 +39,7 @@ export default class Account extends React.Component {
       displayName: null,
       description: null,
       status: null,
-      profilePicUrl: null,
+      profilePicUrl: 'placeholder',
     }
   }
 
@@ -52,39 +54,40 @@ export default class Account extends React.Component {
 
   render () {
     const Header = (() => (
-      <Grid
-        container
-        justify="center"
-        align="center"
-      // TODO: Background gradient, primary -> black
-        style={{width: '100%', background: theme.palette.primary.dark, margin: 0}}
-      >
-        <Grid item container xs={10}>
-          <Card style={{width: PROFILE_IMG_SIZE, margin: MARGIN, display: 'inline-flex'}}>
-            <CardMedia
-              image={this.state.profilePicUrl}
-              title={this.state.username + '\'s profile picture'}
-              style={{height: PROFILE_IMG_SIZE, width: PROFILE_IMG_SIZE}}
-            />
-          </Card>
-          <span style={{textAlign: 'left', margin: MARGIN, display: 'inline-flex', alignItems: 'center'}}>
-            <div>
-              <Typography variant="h5">
-                {this.state.displayName}
-              </Typography>
-              <Typography variant="subtitle1" style={{color: theme.palette.text.hint}}>
-                {this.state.username}
-              </Typography>
-              <Typography variant="subtitle1" style={{color: theme.palette.text.hint}}>
-                {this.state.status}
-              </Typography>
-            </div>
-          </span>
+      <Card style={{width: '100%', background: HEADER_GRADIENT}}>
+        <Grid
+          container
+          justify="center"
+          align="center"
+          style={{width: '100%', margin: 0}}
+        >
+          <Grid item container xs={10}>
+            <Card style={{width: PROFILE_IMG_SIZE, margin: MARGIN, display: 'inline-flex'}}>
+              <CardMedia
+                image={this.state.profilePicUrl}
+                title={this.state.username + '\'s profile picture'}
+                style={{height: PROFILE_IMG_SIZE, width: PROFILE_IMG_SIZE}}
+              />
+            </Card>
+            <span style={{textAlign: 'left', margin: MARGIN, display: 'inline-flex', alignItems: 'center'}}>
+              <div>
+                <Typography variant="h5">
+                  {this.state.displayName}
+                </Typography>
+                <Typography variant="subtitle1" style={{color: theme.palette.text.hint}}>
+                  {this.state.username}
+                </Typography>
+                <Typography variant="subtitle1" style={{color: theme.palette.text.hint}}>
+                  {this.state.status}
+                </Typography>
+              </div>
+            </span>
+          </Grid>
         </Grid>
-      </Grid>
+      </Card>
     ))
 
-    const Details = (() => (
+    const Description = (() => (
       <Grid
         container
         justify="center"
@@ -93,23 +96,34 @@ export default class Account extends React.Component {
         style={{width: '100%', margin: 0}}
       >
         <Grid item container xs={10}>
-          <Paper style={{width: '100%', margin: MARGIN, padding: MARGIN, textAlign: 'left'}}>
+          <Card
+            style={{width: '100%',
+                    margin: MARGIN,
+                    padding: MARGIN,
+                    textAlign: 'left',}}
+          >
             <Typography variant="h6">
               Personal Description
             </Typography>
-            <Typography variant="body">
+            <Typography variant="body1">
               {this.state.description}
             </Typography>
-          </Paper>
+          </Card>
         </Grid>
       </Grid>
     ))
 
     return (
       <div style={{width: '100%', alignItems: 'middle'}}>
-        <Card style={{padding: 0, maxWidth: MAIN_CARD_MAX_WIDTH, margin: 'auto'}}>
+        <Card
+          style={{padding: 0,
+                  maxWidth: MAIN_CARD_MAX_WIDTH,
+                  margin: 'auto',
+                  background: BODY_GRADIENT}}
+        >
+          {/* TODO: Probably should put all "Grid" type stuff here for easier modifications */}
           <Header/>
-          <Details/>
+          <Description/>
         </Card>
       </div>
     );
--- front_end/src/theme.js
+++ front_end/src/theme.js
@@ -30,4 +30,22 @@ const theme = createMuiTheme({
   },
 })
 
+const defaultOpts = {
+  deg: '90deg',
+}
+
+theme.getGradient = (colors, opts) => {
+  if (!opts) opts = {}
+  const optsWithDefaults = Object.assign({}, defaultOpts, opts)
+
+  if (colors.length === 0)
+    return 'rgba(0, 0, 0, 0)'
+  else if (colors.length === 1)
+    return colors[0]
+  else if (colors.length === 2)
+    return `linear-gradient(${optsWithDefaults.deg}, ${colors[0]} 16%, ${colors[1]} 77%`
+  else
+    return `linear-gradient(${optsWithDefaults.deg}, ${colors[0]} 16%, ${colors[1]} 77%, ${colors[2]} 100%)`
+}
+
 export { theme }
