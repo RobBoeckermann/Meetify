5c80a17b65948eb10ba707a54a26bf803eedfada
Jake Steuver <jakesteuver@gmail.com>
2021-02-13 10:25:49 -0500

    Add serializers and remove password from returned user model

---
+++ Django/Meetify/serializers.py
@@ -0,0 +1,30 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+
+from .models import *
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'last_login', 'username', 'email', 'is_active', 'date_joined']
+
+        
+class UserInfoSerializer(serializers.ModelSerializer):
+    User = UserSerializer(read_only=True)
+
+    class Meta:
+        model = User_Info
+        fields = '__all__'
+
+
+class MatchesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Matches
+        fields = '__all__'
+
+
+class MessagesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Liked_Songs
+        fields = '__all__'
